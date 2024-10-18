fa6bd065239ca96892f77e09fef4b7f1d2d6216a
Saylani-GenAIChatbot <highrated109@gmail.com>
2024-10-18 22:48:20 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,210 @@
+// // History of JS
+// // Founder of JS
+// // Brendon eich 1995
+// // Netscape 02
+// // EcmaScript ES6
+
+// // function num(){  ES5
+
+// // }
+
+// // const app = () => {
+
+// // }
+
+// console.log("Hello Pakistan")
+// document.write("<h1> Hello From Web Page</h1> <br><br>")
+// // alert("You are in a web Page")
+// // confirm("Do you want to proceed?")
+
+// prompt("Enter a value")
+
+// // // Variable -->
+// // var a = "Hammad Sheikh" // String
+// // var b = 12 // Number
+// // var bool = true
+// // console.log(a)
+
+// // var num1 = 5
+// // var num2 = 10
+
+// var num1 = +prompt("Enter a value 1")
+// var num2 = +prompt("Enter a value 2")
+
+// console.log(num1 + num2, "The Addition is: ")
+// console.log(num1 - num2, "The Subtraction is: ")
+// console.log(num1 * num2, "The Multiplication is: ")
+// // document.write(number)
+// // console.log(num1 + num2)
+// // document.write(str + "<br><br>")
+// // document.write(num1 + num2)
+
+
+// // Show in the web Page
+
+// // Data Types
+
+// // String
+// // var str = "Pakistan"
+// // var num = 120
+// // var bool1 = true
+// // var bool2 = false
+// // var arr = [12, "Hammad", true]
+
+
+// // console.log(arr)
+// // console.table(bool2)
+
+// // confirm("Are your sure to Proceed")
+// // var inp = prompt("Enter the Number: ")
+
+// // document.write(`${inp} x 1 =`, inp * 1 + "<br>") // For writing the Dynamic value we use "$" with bacticks
+// // document.write(`${inp} x 2 =`, inp * 2 + "<br>")
+// // document.write(`${inp} x 3 =`, inp * 3 + "<br>")
+// // document.write(`${inp} x 4 =`, inp * 4 + "<br>")
+// // document.write(`${inp} x 5 =`, inp * 5 + "<br>")
+// // document.write(`${inp} x 6 =`, inp * 6 + "<br>")
+// // document.write(`${inp} x 7 =`, inp * 7 + "<br>")
+// // document.write(`${inp} x 8 =`, inp * 8 + "<br>")
+// // document.write(`${inp} x 9 =`, inp * 9 + "<br>")
+// // document.write(`${inp} x 10 =`, inp * 10 + "<br>")
+
+
+// // Arithematic operator
+// var sum1 = 10
+// var sum2 = 20
+// var add = sum1 + sum2 //Addition
+// console.log("Addition" + add)
+// var sub = sum1 - sum2
+// console.log("Subtraction" + sub)
+// var mul = sum1 * sum2
+// console.log("Multiplication " + mul)
+// var div = sum1 / sum2
+// console.log(div)
+// var mod = sum1 % sum2
+// console.log(mod)
+
+
+// // console.log("The Result of Addition is : " + add)
+// // console.log("The Result of Subtraction is : " + sub)
+// // console.log("The Result of Multiplication is : " + mul)
+// // console.log("The Result of Divison is : " + div)
+// // console.log("The Result of Modulation is : " + mod)
+
+// // // constant
+// // var a1 = "Jack"
+// // var a1 = "Karachi"
+// // console.log(a1)
+
+// // const data1 = "Computer Science" // Const value can not be re declear
+// // console.log(data1)
+// // // const data1 = "react"
+
+// // let x = 3;
+// // const y = --x;
+// // // ++, --, ===, !
+
+// // const fls = !true
+// // console.log(fls)
+
+// // console.log(y)
+// // // Expected output: "x:4, y:3"
+// // // Expected output: "a:4, b:4"
+
+// // var myName = 'Anus'
+// // console.log(myName)
+
+// // var a = parseInt(prompt('Enter a number'))
+// // var a = Number(prompt('Enter a number'))
+// // var a = +prompt('Enter a number')
+
+// // if (a === 5) {
+// //     console.log('You are right')
+// // }
+// // else {
+// //     console.log('you are wrong')
+// // }
+
+// // Comparison Operator => ===, >, <, >=, <=, !==
+
+// // var a = +prompt('Enter a number')
+
+// // if (a === 2) {
+// //     console.log('You are almost right')
+// // }
+// // else if (a === 3) {
+// //     console.log('You are 99% right')
+// // }
+// // else if (a === 4) {
+// //     console.log('You are 100% right')
+// // }
+// // else {
+// //     console.log('you are wrong')
+// // }
+
+// // var arr = ['one, two', 'three', 'four', 'five']
+// // arr.pop()
+// // arr.push('five')
+// // arr.unshift('Zero')
+// // arr.shift()
+// // arr.splice(2, 0, 'addition')
+// // console.log(arr)
+
+
+
+
+// // var inp = +prompt("Enter your Percentage: ") // 90
+
+// // if (inp >= 80) {
+// //     alert("You Got A-1+")
+
+// // } else if (inp >= 70) {
+// //     alert("You Got A")
+
+// // } else if (inp <= 60) {
+// //     alert("You Got B")
+
+// // } else if (inp <= 50) {
+// //     alert("You Got C")
+
+// // } else if (inp <= 40) {
+// //     alert("You Got D")
+
+// // } else {
+// //     alert("Either you are fail or Please give correct value")
+// // }
+
+// // var numbers = [3, 7, 2, 10, 5, 9, 1, 8, 4, 6]; var largest = numbers[0];
+
+// // for (var i = 1; i < numbers.length; i++) { if (numbers[i] > largest) { largest = numbers[i]; } }
+
+// // console.log(largest); // Output: 10
+
+
+// // var str1 = prompt("How are you?")
+// // console.log(str1)
+
+// var numb = 8
+
+// // if (numb === 5) {
+// //     console.log("It is Okay")
+// // }
+// // else {
+// //     console.log("Wrong number")
+// // }
+
+
+// if (numb === 5) {
+//     console.log("True")
+// } else if (numb > 6) {
+//     console.log("One")
+// }
+// else {
+//     console.log("Two")
+// }
+
+               0    , 1   , 2   , 3
+var arr = ["Hammad", 102.2, true, 1]
+var arr1 = ["jack", "Joe", "Asser"]
+console.log(arr1.reverse())
+console.log(arr)
